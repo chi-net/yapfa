e9316837caa93eef596cc281e095a2ea226d4a8f
chihuo2104 <i@chihuo2104.dev>
2026-05-16 01:22:18 +0800

    fix: missing with httpclient

--- pkg/conn.go
+++ pkg/conn.go
@@ -15,3 +15,9 @@ func Connect(host string) *Conn {
 	cli := yapfa_v1connect.NewYAPFAClient(http.DefaultClient, host)
 	return &Conn{cli: cli}
 }
+
+// ConnectWithHttpClient 带http client内置的连接
+func ConnectWithHttpClient(host string, client *http.Client) *Conn {
+	cli := yapfa_v1connect.NewYAPFAClient(client, host)
+	return &Conn{cli: cli}
+}
